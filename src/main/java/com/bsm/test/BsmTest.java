1acdb992ac0590c620964c7dd223d030f2d6a504
baoshengmin <baoshengmin@fengbangleasing.com>
2018-09-13 13:56:00 +0800

    Hello World

---
+++ src/main/java/com/bsm/test/BsmTest.java
@@ -0,0 +1,15 @@
+package main.java.com.bsm.test;
+
+/**
+ * @Author: baoshengmin
+ * @Date: 2018/9/13 13:39
+ * @Version: 1.0
+ */
+public class BsmTest {
+
+    public static void main(String[] args) {
+         System.out.println("Hello World");
+    }
+
+
+}
